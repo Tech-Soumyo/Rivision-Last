46184100f6d83098df39850378aea93ff3d5339f
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-07-12 20:10:32 +0530

    tailwind setup

--- react_router/tailwind.config.js
+++ react_router/tailwind.config.js
@@ -1,6 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./public/index.html",
+    // Add other paths if needed
+  ],
   theme: {
     extend: {},
   },
